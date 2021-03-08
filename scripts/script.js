82bf9c92caaef0a435279ee0cd9e2e329fdd81c0
Javiistacks <javiann.marrero@icloud.com>
2021-03-07 19:07:11 -0600

    console log working and different color for time

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,106 @@
-let events = JSON.parse(localStorage.getItem('events'));
-$(document).ready(function () {
-    var hour = moment().hours
-    // Getting from local storage...
-    if (events == null) {
-        events = [];
-    }
-    console.log(events);
+// Moment.js
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+// Text hour var
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
 
-    for (let i = 0; i < events.length; i++) {
-        const event = events[i];
-        console.log('Event: ', event)
-        const input = $(`.time-block[data-time="${event.time}"] .input`);
-        input.val(event.event);
-    }
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+// var hourString = $(".hour").text().split(" ");
+
+// Date and Hour
+
+var interval = setInterval(function() {
+  var momentNow = moment();
+  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                      + momentNow.format('dddd')
+                       .substring(0,3).toUpperCase());
+  $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+}, 100);
+
+function initPage() {
+
+  console.log("Current Hour " + hour);
+  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+  nineAm.val(init9);
+
+  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+  tenAm.val(init10);
+  
+  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+  elevenAm.val(init11);
+  
+  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+  twelvePm.val(init12);
+  
+  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+  onePm.val(init1);
+  
+  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+  twoPm.val(init2);
+  
+  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+  threePm.val(init3);
+ 
+  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+  fourPm.val(init4);
+  
+  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+  fivePm.val(init5);
+  
+  var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+  sixPm.val(init6);
+  
+  var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+  sevenPm.val(init7);
+} 
+
+function background () {
+      
+  $(".form-control").each(function () {
+      var timeTest = parseInt($(this).attr("id"));
+      hour = parseInt(hour);
+      console.log(timeTest);
+      console.log(hour);
+//      console.log(this);
+      if (hour > timeTest) {
+          $(this).addClass("past");
+      } else if (hour < timeTest) {
+          $(this).addClass("future");
+      } else {
+          $(this).addClass("present");
+      }
+  });
+}
+
+$(document).ready(function(){
+  initPage()
+  background()
+
+  // Buttons (save to Local Storage)
+  $(".saveBtn").on("click", function(){
+    userInput = $(this).siblings(".form-control").val().trim();
+    console.log(userInput);
+    hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+    console.log(hourSpan);
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
+
+  })
+  // Button for clear the day
+  $("#clearDay").on("click", function(){
+    localStorage.clear();
+    initPage()
+  }) 
 
 });
-
-function getDate() {
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-
-}
-getDate()
-setInterval(getDate, 1000)
-
-$('.save').on('click', function () {
-    // We want to grab the input's value 
-    const event = $(this).siblings('.event-input').children('.input').val();
-    const time = $(this).parent().data('time');
-    console.log(time);
-    console.log(event);
-    const eventObj = {
-        time: time,
-        event: event
-    }
-    const index = events.findIndex(function (event) {
-        return event.time == time;
-    })
-    // if it exists, remove it
-    if (index > -1) {
-        events.splice(index, 1, eventObj);
-    } else {
-        events.push(eventObj);
-    }
-    // Saving to local storage
-    // Save it 
-    // Does it already exist??
-    console.log('it already exists at position ', index);
-
-    localStorage.setItem('events', JSON.stringify(events))
-
-})
-
-var planner = document.getElementById("planner")
